2a9d7ca5f1ab6e2e1bec8ff977423a45f46a553e
Gobuub <botica1982@msn.com>
2024-01-20 22:21:19 +0100

    Add English version to Hello page

--- Hello.py
+++ Hello.py
@@ -16,7 +16,7 @@ st.sidebar.success("## Menú")
 
 st.markdown(
     """
-    En el menú lateral podreas navegar por las diferentes páginas de la aplicación, en próximas iteraciones
+    En el menú lateral podrás navegar por las diferentes páginas de la aplicación, en próximas iteraciones
     iremos añadiendo más páginas.
     
     ### ¿Qué contenido encontraremos en FIFA 23 Scouting APP?
@@ -28,6 +28,24 @@ st.markdown(
     
     """
 )
+
+st.divider()
+
+st.markdown(
+    """
+    On the sidebar menu you can navigate through the different pages of the app, in the future we will add new 
+    pages to the app.
+    
+    ### What type of content will you find on FIFA 23 Scouting APP?
+    - Dashboards with the stats of all players included on FIFA 23
+    - An application where you can find the possibles substitutes for a player from any team (until now
+      we return the top five profiles of possibles substitutes, in the future we will return more players, and
+      you can add some kind of filters like age, prices, overall, etc.)
+    - An application to compare players stats when you find a possible substitute for your player.
+    
+    """
+)
+
 
 image_url = 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/57/Easports_fifa_logo.svg/800px-Easports_fifa_logo.svg.png'
 
